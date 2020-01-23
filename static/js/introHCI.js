f974b36533b786bf67716ab0e0e2c54e8f7a025c
thomastangerine <thomastang.thomas97@gmail.com>
2020-01-22 23:19:48 -0800

    done

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,21 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
 	});
 
+	$("a.thumbnail").click(function projectClick(e) {
+		e.preventDefault();
+		var containingProject = $(this).closest(".project");
+		var description = $(containingProject).find(".project-description");
+		if (description.length == 0) {
+		   $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+		} else {
+		   description.fadeOut();
+		}
+	});
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
+
